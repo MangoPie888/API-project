16b09b7f108fc25443caaebc22c1f944438b56e1
vivian wang <viviannewang_dy@yahoo.com>
2022-09-28 10:42:32 -0700

    complete get all spots owned by the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,7 @@
 const express = require('express');
 // const sequelize = require('sequelize')
 const {Spot, SpotImage, Review, Sequelize, sequelize} = require('../../db/models');
+const{setTokenCookie, restoreUser,requireAuth} = require('../../utils/auth');
 
 const router = express.Router();
 
@@ -53,7 +54,59 @@ router.get('/', async(req,res,next) =>{
 });
 
 
+//Get all Spots owned by the Current User
+router.get('/current', restoreUser,requireAuth,async(req,res,next) =>{
+    const {user} = req;
+    if(user) {
+        const spots = await Spot.findAll({
+            where:{ownerId:user.id},
+            include:{
+                model:SpotImage,
+                attributes:['url']
+            }
+        });
+    
+        const Spots = [];
+    
+        for(let spot of spots) {
+            const average = await Review.findAll({
+                where:{spotId:spot.id},
+                attributes:[
+                    [
+                        sequelize.fn("AVG", sequelize.col('stars')), 
+                        "avgRating"
+                    ]
+                ],
+                raw:true
+            });
+    
+            const spotsBody = {
+                id:spot.id,
+                ownerId: spot.ownerId,
+                address: spot.address,
+                city:spot.city,
+                state:spot.state,
+                country:spot.country,
+                lat:spot.lat,
+                lng:spot.lng,
+                name:spot.name,
+                description:spot.description,
+                price:spot.price,
+                createdAt:spot.createdAt,
+                updatedAt:spot.updatedAt,
+                aveRating:average[0].avgRating,
+                previewImage:spot.SpotImages[0].url
+            };
+            Spots.push(spotsBody)
+        };
+    
+        res.json({
+        Spots
+        })
 
+    }else return res.json({})
+
+});
 
 
 module.exports = router;
